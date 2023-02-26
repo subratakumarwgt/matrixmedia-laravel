d5f01194ee77df0e4098bf97655403498d8c579e
subratakumarwgt <subratakumar.wgt@gmail.com>
2023-02-26 10:51:57 +0530

    Final touches

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,30 +20,16 @@
             }
         </style>
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                   <h5><a href="/projects"></a></h5>
-                   <h5><a href="/tasks"></a></h5>
-                </div>
-
-          
-            </div>
-        </div>
+    <body class="">
+        <center>
+            <h4>Matrix Media | Assignment</h4>
+            <hr>
+       
+            <a href="/projects" target="_blank"><h5> Go to projects </h5></a>
+            <a href="/tasks"  target="_blank"><h5>Go to tasks </h5></a>
+            <a href="/routes"  target="_blank"><h5>Go to Routes </h5></a>
+        </center>
+        
+   
     </body>
 </html>
